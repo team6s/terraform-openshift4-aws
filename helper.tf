6e2bb3f46ebd4897f74b45ddfea8450456942c61
known0known <121076225+known0known@users.noreply.github.com>
2023-08-14 09:31:29 +0000

    xx

--- helper.tf
+++ helper.tf
@@ -1,12 +1,13 @@
 locals {
   major_version   = join(".", slice(split(".", var.openshift_version), 0, 2))
   aws_azs         = (var.aws_azs != null) ? var.aws_azs : tolist([join("",[var.aws_region,"a"]),join("",[var.aws_region,"b"]),join("",[var.aws_region,"c"])])
-  rhcos_image     = lookup(lookup(lookup(jsondecode(data.http.images.body), "amis"), "${var.aws_region}"),"hvm")
+  # rhcos_image     = lookup(lookup(lookup(jsondecode(data.http.images.body), "amis"), "${var.aws_region}"),"hvm")
+  rhcos_image = "ami-001ad079c331d2612"
 }
 
-data "http" "images" {
-  url = "https://raw.githubusercontent.com/openshift/installer/release-${local.major_version}/data/data/rhcos.json"
-  request_headers = {
-    Accept = "application/json"
-  }
-}
+# data "http" "images" {
+#   url = "https://raw.githubusercontent.com/openshift/installer/release-${local.major_version}/data/data/rhcos.json"
+#   request_headers = {
+#     Accept = "application/json"
+#   }
+# }
